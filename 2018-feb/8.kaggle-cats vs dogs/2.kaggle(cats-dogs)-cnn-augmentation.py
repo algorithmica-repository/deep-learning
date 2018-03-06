378fc67673fb186bf1cf9a3a68a1c15e719a8a46
Algorithmica <info@algorithmica.co.in>
2018-03-06 17:51:02 +0530

    uploading updated code

--- 2018-feb/8.kaggle-cats vs dogs/2.kaggle(cats-dogs)-cnn-augmentation.py
+++ 2018-feb/8.kaggle-cats vs dogs/2.kaggle(cats-dogs)-cnn-augmentation.py
@@ -3,26 +3,23 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Dropout, Flatten, Dense
+from keras.layers import Flatten, Dense, Dropout
 from keras import backend as K
+#import collections
 import os
 import pandas as pd
 import utils
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 
-
-os.getcwd()
-train_dir, validation_dir, test_dir = utils.prepare_data_flow_directory(
+train_dir, validation_dir, test_dir, nb_train_samples, nb_validation_samples,nb_test_samples = \
+                    utils.preapare_small_dataset_for_flow(
                             train_dir_original='C:\\Users\\data\\train', 
                             test_dir_original='C:\\Users\\data\\test',
                             target_base_dir='C:\\Users\\Thimma Reddy\\data1')
 
 img_width, img_height = 150, 150
-nb_train_samples = 2000
-nb_validation_samples = 1000
-nb_test_samples = 12500
-epochs = 2
-batch_size = 32
+epochs = 100
+batch_size = 20
 
 if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
@@ -39,16 +36,14 @@ model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
-model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
 print(model.summary())
 
-
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
+model.compile(loss='binary_crossentropy', 
+              optimizer='adam',
               metrics=['accuracy'])
-
 
 train_datagen = ImageDataGenerator(
         rescale=1./255,
@@ -59,22 +54,20 @@ train_datagen = ImageDataGenerator(
         zoom_range=0.2,
         horizontal_flip=True,
         fill_mode='nearest')
-
 validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='binary')
-
+    class_mode='categorical')
 validation_generator = validation_datagen.flow_from_directory(
     validation_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=2, verbose=1, mode='auto')   
+early_stopping = EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto')   
 save_weights = ModelCheckpoint('model.h5', monitor='val_loss', save_best_only=True)
 
 history = model.fit_generator(
@@ -90,21 +83,21 @@ historydf = pd.DataFrame(history.history, index=history.epoch)
 utils.plot_loss_accuracy(history)
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
-
 test_generator = test_datagen.flow_from_directory(
         test_dir,
         target_size=(img_width, img_height),
         batch_size=batch_size,
         class_mode=None,
         shuffle=False)
-print(test_generator.filenames)
+#print(test_generator.filenames)
 probabilities = model.predict_generator(test_generator, nb_test_samples//batch_size)
 
 mapper = {}
 i = 0
 for file in test_generator.filenames:
     id = int(file.split('\\')[1].split('.')[0])
-    mapper[id] = probabilities[i][0]
+    mapper[id] = probabilities[i][1]
     i += 1
-tmp = pd.DataFrame({'id':list(mapper.keys()),'label':list(mapper.values())})
-tmp.to_csv('submission.csv', columns=['id','label'],index=False)
+#od = collections.OrderedDict(sorted(mapper.items()))    
+tmp = pd.DataFrame({'id':list(mapper.keys()),'label':list(mapper.values())})    
+tmp.to_csv('submission.csv', columns=['id','label'], index=False)
