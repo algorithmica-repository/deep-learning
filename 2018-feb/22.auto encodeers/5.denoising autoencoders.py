d44523a1f76ce30b6c089cd6524da85f90b0c86d
Algorithmica <info@algorithmica.co.in>
2018-05-22 23:08:28 +0530

    class code

--- 2018-feb/22.auto encodeers/5.denoising autoencoders.py
+++ 2018-feb/22.auto encodeers/5.denoising autoencoders.py
@@ -3,6 +3,7 @@ from keras.models import Model
 from keras.datasets import mnist 
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.callbacks import ModelCheckpoint
 
 def plot_noisy(n, X):
     plt.figure(figsize=(20, 4))
@@ -36,40 +37,46 @@ def plot(n, X, Decoded_X):
 (X_train, _), (X_test, _) = mnist.load_data()
 X_train = X_train.astype('float32') / 255.
 X_test = X_test.astype('float32') / 255.
-X_train = X_train[..., np.newaxis]
-X_test = X_test[..., np.newaxis]
 
-noise_factor = 0.5
-X_train_noisy = X_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=X_train.shape) 
-X_test_noisy = X_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=X_test.shape) 
-plot_noisy(10, X_train_noisy)
+noise_factor = 0.25
+X_train_noisy = X_train + noise_factor * np.random.normal(size=X_train.shape) 
+X_test_noisy = X_test + noise_factor * np.random.normal(size=X_test.shape) 
 
 X_train_noisy = np.clip(X_train_noisy, 0., 1.)
 X_test_noisy = np.clip(X_test_noisy, 0., 1.)
 
+X_train = X_train[..., np.newaxis]
+X_test = X_test[..., np.newaxis]
+X_train_noisy = X_train_noisy[..., np.newaxis]
+X_test_noisy = X_test_noisy[..., np.newaxis]
+plot_noisy(10, X_train_noisy)
+
 input_size = 784
-epochs = 10
-batch_size = 256
+epochs = 30
+batch_size = 128
 
 
 input_img = Input(shape=(28, 28, 1)) 
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
 x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
 encoded = MaxPooling2D((2, 2), padding='same')(x)
 
 # at this point the representation is (7, 7, 32)
 
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
+x = Conv2DTranspose(64, (3, 3), activation='relu', padding='same')(encoded)
 x = UpSampling2D((2, 2))(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+x = Conv2DTranspose(32, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
-decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+decoded = Conv2DTranspose(1, (3, 3), activation='sigmoid', padding='same')(x)
 
 autoencoder = Model(input_img, decoded)
+print(autoencoder.summary())
 
-autoencoder.compile(optimizer='adam', loss='mean_squared_error')
-autoencoder.fit(X_train_noisy, X_train, epochs=epochs, batch_size=batch_size, shuffle=True, validation_split=0.2)
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+save_weights = ModelCheckpoint('model.h5', monitor='val_loss', save_best_only=True)
+autoencoder.fit(X_train_noisy, X_train, epochs=epochs, batch_size=batch_size, 
+                shuffle=True, validation_split=0.2, callbacks=[save_weights])
 
 decoded_imgs = autoencoder.predict(X_test_noisy)
 plot(10, X_test_noisy, decoded_imgs)
