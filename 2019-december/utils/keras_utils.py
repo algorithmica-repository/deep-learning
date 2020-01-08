070063a85d3904e48a185f2dd393536e5b8b0f4c
Algorithmica <info@algorithmica.co.in>
2020-01-08 17:26:42 +0530

    Add files via upload

--- 2019-december/utils/keras_utils.py
+++ 2019-december/utils/keras_utils.py
@@ -1,32 +1,257 @@
 import matplotlib.pyplot as plt
+import math
+from PIL import Image
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from collections import OrderedDict
+import keras.backend as K
+from keras.models import Model
+import os
 
-def plot_loss_accuracy(history):
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
-    epochs = range(len(history.epoch))
-    plt.plot(epochs, acc, 'bo', label='Training acc')
-    plt.plot(epochs, val_acc, 'b', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-    
+def n_(node, output_format_):
+    node_name = str(node.name)
+    if output_format_ == 'simple':
+        if '/' in node_name:
+            return node_name.split('/')[0]
+        elif ':' in node_name:
+            return node_name.split(':')[0]
+        else:
+            return node_name
+    return node_name
+
+def _evaluate(model: Model, nodes_to_evaluate, x, y=None, auto_compile=False):
+    if not model._is_compiled:
+        if model.name in ['vgg16', 'vgg19', 'inception_v3', 'inception_resnet_v2', 'mobilenet_v2', 'mobilenetv2']:
+            print('Transfer learning detected. Model will be compiled with ("categorical_crossentropy", "adam").')
+            print('If you want to change the default behaviour, then do in python:')
+            print('model.name = ""')
+            print('Then compile your model with whatever loss you want: https://keras.io/models/model/#compile.')
+            print('If you want to get rid of this message, add this line before calling keract:')
+            print('model.compile(loss="categorical_crossentropy", optimizer="adam")')
+            model.compile(loss='categorical_crossentropy', optimizer='adam')
+        else:
+            if auto_compile:
+                model.compile(loss='mse', optimizer='adam')
+            else:
+                print('Please compile your model first! https://keras.io/models/model/#compile.')
+                print('If you only care about the activations (outputs of the layers), '
+                      'then just compile your model like that:')
+                print('model.compile(loss="mse", optimizer="adam")')
+                raise Exception('Compilation of the model required.')
+
+    def eval_fn(k_inputs):
+        return K.function(k_inputs, nodes_to_evaluate)(model._standardize_user_data(x, y))
+
+    try:
+        return eval_fn(model._feed_inputs + model._feed_targets + model._feed_sample_weights)
+    except Exception:
+        return eval_fn(model._feed_inputs)
+
+def get_activations(model, x, layer_name=None, nodes_to_evaluate=None,
+                    output_format='simple', auto_compile=True):
+    if nodes_to_evaluate is None:
+        nodes = [layer.output for layer in model.layers if layer.name == layer_name or layer_name is None]
+    else:
+        if layer_name is not None:
+            raise ValueError('Do not specify a [layer_name] with [nodes_to_evaluate]. It will not be used.')
+        nodes = nodes_to_evaluate
+
+    if len(nodes) == 0:
+        if layer_name is not None:
+            network_layers = ', '.join([layer.name for layer in model.layers])
+            raise KeyError('Could not find a layer with name: [{}]. '
+                           'Network layers are [{}]'.format(layer_name, network_layers))
+        else:
+            raise ValueError('Nodes list is empty. Or maybe the model is empty.')
+
+    # The placeholders are processed later (Inputs node in Keras). Due to a small bug in tensorflow.
+    input_layer_outputs, layer_outputs = [], []
+    [input_layer_outputs.append(node) if 'input_' in node.name else layer_outputs.append(node) for node in nodes]
+    activations = _evaluate(model, layer_outputs, x, y=None, auto_compile=auto_compile)
+
+    def craft_output(output_format_):
+        activations_dict = OrderedDict(zip([n_(output, output_format_) for output in layer_outputs], activations))
+        activations_inputs_dict = OrderedDict(zip([n_(output, output_format_) for output in input_layer_outputs], x))
+        result_ = activations_inputs_dict.copy()
+        result_.update(activations_dict)
+        if output_format_ == 'numbered':
+            result_ = OrderedDict([(i, v) for i, (k, v) in enumerate(result_.items())])
+        return result_
+
+    result = craft_output(output_format)
+    if nodes_to_evaluate is not None and len(result) != len(nodes_to_evaluate):
+        result = craft_output(output_format_='full')  # collision detected in the keys.
+
+    return result
+
+def display_heatmaps(activations, input_image, directory='.', save=False, fix=True):
+    data_format = K.image_data_format()
+    if fix:
+        # fixes common errors made when passing the image
+        # I recommend the use of keras' load_img function passed to np.array to ensure
+        # images are loaded in in the correct format
+        # removes the batch size from the shape
+        if len(input_image.shape) == 4:
+            input_image = input_image.reshape(input_image.shape[1], input_image.shape[2], input_image.shape[3])
+        # removes channels from the shape of grayscale images
+        if len(input_image.shape) == 3 and input_image.shape[2] == 1:
+            input_image = input_image.reshape(input_image.shape[0], input_image.shape[1])
+
+    index = 0
+    for layer_name, acts in activations.items():
+        print(layer_name, acts.shape, end=' ')
+        if acts.shape[0] != 1:
+            print('-> Skipped. First dimension is not 1.')
+            continue
+        if len(acts.shape) <= 2:
+            print('-> Skipped. 2D Activations.')
+            continue
+        print('')
+        nrows = int(math.sqrt(acts.shape[-1]) - 0.001) + 1  # best square fit for the given number
+        ncols = int(math.ceil(acts.shape[-1] / nrows))
+        fig, axes = plt.subplots(nrows, ncols, figsize=(12, 12))
+        fig.suptitle(layer_name)
+
+        # computes values required to scale the activations (which will form our heat map) to be in range 0-1
+        scaler = MinMaxScaler()
+        # reshapes to be 2D with an automaticly calculated first dimension and second
+        # dimension of 1 in order to keep scikitlearn happy
+        scaler.fit(acts.reshape(-1, 1))
+
+        # loops over each filter/neuron
+        for i in range(nrows * ncols):
+            if i < acts.shape[-1]:
+                if len(acts.shape) == 3:
+                    # gets the activation of the ith layer
+                    if data_format == 'channels_last':
+                        img = acts[0, :, i]
+                    elif data_format == 'channels_first':
+                        img = acts[0, i, :]
+                    else:
+                        raise Exception('Unknown data_format.')
+                elif len(acts.shape) == 4:
+                    if data_format == 'channels_last':
+                        img = acts[0, :, :, i]
+                    elif data_format == 'channels_first':
+                        img = acts[0, i, :, :]
+                    else:
+                        raise Exception('Unknown data_format.')
+                else:
+                    raise Exception('Expect a tensor of 3 or 4 dimensions.')
+
+                # scales the activation (which will form our heat map) to be in range 0-1 using
+                # the previously calculated statistics
+                if len(img.shape) == 1:
+                    img = scaler.transform(img.reshape(-1, 1))
+                else:
+                    img = scaler.transform(img)
+                # print(img.shape)
+                img = Image.fromarray(img)
+                # resizes the activation to be same dimensions of input_image
+                img = img.resize((input_image.shape[0], input_image.shape[1]), Image.LANCZOS)
+                img = np.array(img)
+                axes.flat[i].imshow(input_image / 255.0)
+                # overlay the activation at 70% transparency  onto the image with a heatmap colour scheme
+                # Lowest activations are dark, highest are dark red, mid are yellow
+                axes.flat[i].imshow(img, alpha=0.3, cmap='jet', interpolation='bilinear')
+            axes.flat[i].axis('off')
+        if save:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            output_filename = os.path.join(directory, '{0}_{1}.png'.format(index, layer_name.split('/')[0]))
+            plt.savefig(output_filename, bbox_inches='tight')
+        else:
+            plt.show()
+        index += 1
+        plt.close(fig)
+        
+def display_activations(activations, cmap=None, save=False, directory='.', data_format='channels_last'):
+    index = 0
+    for layer_name, acts in activations.items():
+        print(layer_name, acts.shape, end=' ')
+        if acts.shape[0] != 1:
+            print('-> Skipped. First dimension is not 1.')
+            continue
+
+        print('')
+        # channel first
+        if data_format == 'channels_last':
+            c = -1
+        elif data_format == 'channels_first':
+            c = 1
+        else:
+            raise Exception('Unknown data_format.')
+
+        nrows = int(math.sqrt(acts.shape[c]) - 0.001) + 1  # best square fit for the given number
+        ncols = int(math.ceil(acts.shape[c] / nrows))
+        hmap = None
+        if len(acts.shape) <= 2:
+            """
+            print('-> Skipped. 2D Activations.')
+            continue
+            """
+            # no channel
+            fig, axes = plt.subplots(1, 1, squeeze=False, figsize=(24, 24))
+            img = acts[0, :]
+            hmap = axes.flat[0].imshow([img], cmap=cmap)
+            axes.flat[0].axis('off')
+        else:
+            fig, axes = plt.subplots(nrows, ncols, squeeze=False, figsize=(24, 24))
+            for i in range(nrows * ncols):
+                if i < acts.shape[c]:
+                    if len(acts.shape) == 3:
+                        if data_format == 'channels_last':
+                            img = acts[0, :, i]
+                        elif data_format == 'channels_first':
+                            img = acts[0, i, :]
+                        else:
+                            raise Exception('Unknown data_format.')
+                        hmap = axes.flat[i].imshow([img], cmap=cmap)
+                    elif len(acts.shape) == 4:
+                        if data_format == 'channels_last':
+                            img = acts[0, :, :, i]
+                        elif data_format == 'channels_first':
+                            img = acts[0, i, :, :]
+                        else:
+                            raise Exception('Unknown data_format.')
+                        hmap = axes.flat[i].imshow(img, cmap=cmap)
+                axes.flat[i].axis('off')
+        fig.suptitle(layer_name)
+        fig.subplots_adjust(right=0.8)
+        cbar = fig.add_axes([0.85, 0.15, 0.03, 0.7])
+        fig.colorbar(hmap, cax=cbar)
+        if save:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            output_filename = os.path.join(directory, '{0}_{1}.png'.format(index, layer_name.split('/')[0]))
+            plt.savefig(output_filename, bbox_inches='tight')
+        else:
+            plt.show()
+        index += 1
+        plt.close(fig)
+
+def plot_accuracy(history, viz=True):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    print("train accuracy:", acc[-1])
+    print("validation accuracy", val_acc[-1])
+    if viz:
+        plt.figure()
+        epochs = range(len(history.epoch))
+        plt.plot(epochs, acc, 'bo', label='Training acc')
+        plt.plot(epochs, val_acc, 'b', label='Validation acc')
+        plt.title('Training and validation accuracy')
+        plt.legend()
+        plt.show()
+
+def plot_loss(history):
+    plot_accuracy(history, False)
     plt.figure()
     loss = history.history['loss']
     val_loss = history.history['val_loss']
+    epochs = range(len(history.epoch))
     plt.plot(epochs, loss, 'bo', label='Training loss')
     plt.plot(epochs, val_loss, 'b', label='Validation loss')
     plt.title('Training and validation loss')
     plt.legend()
-
-    plt.show()
-
-def plot_loss(history):  
-    plt.figure()
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    epochs = range(len(history.epoch))
-    plt.plot(epochs, loss, 'bo', label='Training loss')
-    plt.plot(epochs, val_loss, 'b', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.legend()
-
     plt.show()
